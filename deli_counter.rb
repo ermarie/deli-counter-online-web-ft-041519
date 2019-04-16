ca7f2c1a56ac84cb2f6b01440f1a9bbda8af044a
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 02:04:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,9 +8,8 @@ def line(katz_deli)
     string = "The line is currently:"
 
     katz_deli.each_with_index do |name, index|
-      string << " #{index + 1}. #{name},"
+      string << " #{index + 1}. #{name}"
     end
-    string.chomp(",")
     string << "."
     puts string
   end
