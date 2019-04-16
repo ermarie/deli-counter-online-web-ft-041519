a713e3773059ebfd297fdcbc00aa500c28483c6d
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 01:53:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "${name} ${katz_deli.length}"
+  puts "WElcome, ${name}. You are number ${katz_deli.length} in line."
 end
 
 def now_serving(array)
