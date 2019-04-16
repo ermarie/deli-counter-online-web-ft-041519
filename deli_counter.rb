e4a8611d5f8b8e20be410fa73042d91e6fd7f9bd
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 01:42:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(katz_deli)
   if katz_deli.empty == true
     "The line is currently empty."
   else
-
+    string = "The line is currently: "
+    katz_deli.each_with_index |name, index|
   end
 end
 
